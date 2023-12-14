e2bcecd5dfcd26b68d084dbe2a7dbb47c82b25af
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2023-12-14 18:19:43 +0530

    initial commit

---
+++ app/src/main/java/com/github/spaciouscoder78/courseweb/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.github.spaciouscoder78.courseweb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.*
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity(){
+    //creating a webview object
+    private lateinit var webView:WebView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //looking for the webview id from android main
+        webView = findViewById(R.id.webview)
+        //transferring the control to webviewclient
+        webView.webViewClient = WebViewClient()
+        //loading the url
+        webView.loadUrl("https://kitsw.rlabs.co.in/")
+        //enabling javascript so it actually works
+        webView.settings.javaScriptEnabled=true
+        //adding zoom support
+        webView.settings.setSupportZoom(true)
+
+    }
+    override fun onBackPressed(){
+        //allowing user to go back
+        if(webView.canGoBack())
+            webView.goBack()
+        else
+            super.onBackPressed()
+    }
+}
